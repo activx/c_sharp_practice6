253927c5ae7423489dd0cb883b03776231dd5784
activx <ilbaev@ator.com.ua>
2015-09-15 15:32:56 +0300

    Ok

---
+++ Complex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practic6
+{
+
+
+    class Complex
+    {
+        double re { get; set; }
+        double im { get; set; }
+
+        public Complex (double real, double im){
+            re = real;
+            this.im = im;
+        }
+
+        public static Complex operator + (Complex numberFirst, Complex numberSecond)
+        {
+            return new Complex(numberFirst.re + numberSecond.re, numberFirst.im + numberSecond.im);
+        }
+
+        public static Complex operator - (Complex numberFirst, Complex numberSecond)
+        {
+            return new Complex(numberFirst.re - numberSecond.re, numberFirst.im - numberSecond.im);
+        }
+
+        public String getObjectAsString(){
+            return  "(" + re + ";" + im + "i)" ;
+        }
+
+        
+    }
+
+}
